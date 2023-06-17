17374a2d2be8f2c1cebbf46394329caa0ae94ac5
Hagertaha1 <hagertaha16@gmail.com>
2023-06-16 19:38:57 -0700

    task compeleted

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #!/bin/bash
-#include <stdio.h>
+ #include <stdio.h>
 
 /**
  * main - entry point
  *
  * description: a c program that prints with put function
  *
- * return : always 0 (sucess)
+ * return : always 0(sucess)
  */
 
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-        return(0); 
+	puts ( "\"Programming is like building a multilingual puzzle" );
+        return (0); 
 } 
