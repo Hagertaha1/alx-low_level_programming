64eb08283420ea87fb7ec721ac6b38f86ce4922a
Hagertaha1 <hagertaha16@gmail.com>
2023-06-17 10:42:55 -0700

    task compeleted

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: c program that prints the size of various types on the computer
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{   sizeof(char a);
+	sizeof(int b);
+return (0);
+}
