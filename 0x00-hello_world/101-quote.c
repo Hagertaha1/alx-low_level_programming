71c7af610731801aa2952040b80955b9c270a087
Hagertaha1 <hagertaha16@gmail.com>
2023-06-19 07:02:55 -0700

    task compeleted

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: c program that prints with git_string function
+ *
+ * Return: always 1 (success)
+*/
+
+int main(void)
+{   get_string("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+    return (1);
+}
