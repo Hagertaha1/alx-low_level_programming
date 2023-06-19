89f417b9da058151fd5ddd8747fb360bdaf52bc6
Hagertaha1 <hagertaha16@gmail.com>
2023-06-19 12:04:40 -0700

    task compeleted

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: c program that prints random numbers
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{   int n=0;
+if(n>0)
+printf("%i is positive", n)
+else if(n<0)
+printf("%i is negative", n)
+else
+printf("%i is zero", n)
+return (0);
+}
