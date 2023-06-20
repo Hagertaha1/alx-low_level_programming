4636889671155e62a9256ef265d5878d6870bdf0
Hagertaha1 <hagertaha16@gmail.com>
2023-06-20 11:44:53 -0700

    task compeleted

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: c program that prints
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{   printf("_putchar\n");
+	return (0);
+}
