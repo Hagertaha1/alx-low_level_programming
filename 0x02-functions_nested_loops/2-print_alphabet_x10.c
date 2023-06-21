baf681b453ed51772322d1b54f2ce2a749f8b89e
Hagertaha1 <hagertaha16@gmail.com>
2023-06-20 19:57:46 -0700

    task compeleted

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints 10 times the alphabet
+*/
+
+void print_alphabet_x10(void)
+{int line, ch;
+for (line = 0; line <= 9; line++)
+_putchar(ch);
+_putchar('\n');
+}
