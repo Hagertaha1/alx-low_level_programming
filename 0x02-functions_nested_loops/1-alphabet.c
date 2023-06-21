7baf4abfa23a89fe2263785c648818e182b15176
Hagertaha1 <hagertaha16@gmail.com>
2023-06-20 19:00:25 -0700

    task compeleted

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: c program makes function for printing
+ *
+ * Return: 0 (success)
+*/
+
+int main(void)
+{int ch;
+for (ch == 'a', ch <= 'z', ch++)
+_putchar(ch);
+_putchar('\n');
+return (0);
+}
