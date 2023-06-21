4c8e34a4ae9031dd814522873b4fdf6e3e4a3aeb
Hagertaha1 <hagertaha16@gmail.com>
2023-06-21 15:31:57 -0700

    task compeleted

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,14 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints 10 times the alphabet
+ * print_alphabet_x10 - function that Prints 10 times the alphabet in lowercse
 */
 
 void print_alphabet_x10(void)
-{
-int line, ch;
+{ int line, ch;
 for (line = 0; line <= 9; line++)
 {
-for (ch = 'a'; ch = 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch);
 _putchar('\n');
 }
