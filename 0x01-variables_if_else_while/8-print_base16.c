cc7fc671c159beb5280b0bf669e3ee789b729db8
Hagertaha1 <hagertaha16@gmail.com>
2023-06-22 11:47:47 -0700

    task compeleted

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program that prints all the numbers of base 16 in lowercase
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{int n;
+char ch;
+for (n = 0; n < 10; n++)
+putchar(n + '0');
+for (ch = 'a'; ch <= f; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
