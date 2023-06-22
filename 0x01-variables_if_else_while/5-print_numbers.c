5e62ab2bd517afb355aa51f408a649ae38058a73
Hagertaha1 <hagertaha16@gmail.com>
2023-06-22 10:15:44 -0700

    task compeleted

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  * main - Entry point
- * 
- * Description: program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Description: program that prints all single digit numbers of 10 start from 0
  *
  * Return: always 0 (success)
 */
