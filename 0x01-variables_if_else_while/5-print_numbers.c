aab1433c7b8f3a10b060cc7b1911f10a2c3d4960
Hagertaha1 <hagertaha16@gmail.com>
2023-06-22 10:14:21 -0700

    task compeleted

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 
+ * Description: program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{ int n;
+while (n < 10)
+printf("%i", n);
+return (0);
+}
