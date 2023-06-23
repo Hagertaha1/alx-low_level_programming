c8cbc1113bf7b4c847f08ef1e489b18da501d0b2
Hagertaha1 <hagertaha16@gmail.com>
2023-06-22 19:48:54 -0700

    task done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers (0 - 9)
+ *
+ * Description: prints the inputs
+ *
+ * Return: return numbers
+*/
+
+void print_numbers(void)
+{int n = 0;
+while (n < 10)
+{_putchar(n);
+}
+_putchar('\n');
+}
