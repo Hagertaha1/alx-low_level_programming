54bab9f7152fe26901f1e0ba18d797de0eaa46ea
Hagertaha1 <hagertaha16@gmail.com>
2023-06-27 00:47:22 -0700

    task done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy string pointed to by src
+ * @dest: charcter type string
+ * @src: charcter type string
+ * Description: copy string pointed to by pointer src to the buffer to dest
+ * Return: pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{int i = -1;
+do(i++;
+dest[i] = scr[i];
+)while (src[i] != '\0')
+return (dest);
+}
